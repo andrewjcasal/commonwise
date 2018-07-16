ebd87ed72cea909c45c248a462e3e4a9a28141bf
Andrew Casal <andrewjcasal@gmail.com>
2018-07-16 12:36:13 -0700

    add JS

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+(function( $ ) {
+  'use strict';
+
+  $(function() {
+
+      $('form#mc-embedded-subscribe-form').submit(function (e) {
+          e.preventDefault();
+          var url = $(this).attr('action');
+
+          $.ajax({
+              url: url,
+              type: 'GET',
+              data: $('#mc-embedded-subscribe-form').serialize(),
+              dataType: 'jsonp',
+              contentType: "application/json; charset=utf-8",
+              success: function (data) {
+                  if (data['result'] != "success") {
+                      //$('#message').text('<p>'+data['msg']+'</p>');
+                      $('#message').html('<p class="coerror">'+data['msg']+'</p>');
+                  } else {
+                      $('#message').html('<p>Thank you. You will get a confirmation email that you must respond to.</p>');
+                  }
+              }
+          });
+      });
+  });
+})( jQuery );
+
+// $(document).ready( function () {
+//     // I only have one form on the page but you can be more specific if need be.
+//     var $form = $('form');
+//
+//     if ( $form.length > 0 ) {
+//         $('form input[type="submit"]').bind('click', function ( event ) {
+//             if ( event ) event.preventDefault();
+//             // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+//             if ( validate_input($form) ) { register($form); }
+//         });
+//     }
+// });
+//
+// function register($form) {
+//     $.ajax({
+//         type: $form.attr('method'),
+//         url: $form.attr('action'),
+//         data: $form.serialize(),
+//         cache       : false,
+//         dataType    : 'json',
+//         contentType: "application/json; charset=utf-8",
+//         error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+//         success     : function(data) {
+//             if (data.result != "success") {
+//                 // Something went wrong, do something to notify the user. maybe alert(data.msg);
+//             } else {
+//                 // It worked, carry on...
+//             }
+//         }
+//     });
+// }
